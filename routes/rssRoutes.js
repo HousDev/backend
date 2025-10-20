738a1002696b221939f2d1ca1406f0da38a64489
Kamlesh7049 <kamleshprasadshah56@gmail.com>
2025-10-20 11:34:41 +0530

    work on blogs

--- routes/rssRoutes.js
+++ routes/rssRoutes.js
@@ -1,25 +1,3 @@
-// // backend/routes/rssRoutes.js
-// const express = require('express');
-// const ctrl = require('../controllers/rssController');
-// const router = express.Router();
-
-// router.get('/', ctrl.list);
-// router.post('/', ctrl.create);
-
-// router.get('/validate', ctrl.validate);
-// router.get('/proxy', ctrl.proxy);
-// router.post('/sync-all', ctrl.syncAll);
-
-// router.post('/:id/sync', ctrl.syncOne);
-// router.patch('/:id/toggle', ctrl.toggle);
-
-// router.get('/:id', ctrl.getById);
-// router.put('/:id', ctrl.update);
-// router.delete('/:id', ctrl.remove);
-
-// module.exports = router;
-
-
 const express = require("express");
 const ctrl = require("../controllers/rssController");
 const router = express.Router();
